e2df2e1ed7b47aa7b981b45a882d727cc857ffe7
Said Elimam <said.elimam@workday.com>
2025-10-28 03:59:51 +0100

    disable home animations

--- src/js/main.js
+++ src/js/main.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', async function () {
 
   initializeSmoothScrolling();
   initializeHeaderScrollEffect();
-  initializeAnimations();
+  // initializeAnimations(); // Disabled animations on home
   initializeScrollToTop();
   initializeVideoAccordion();
 
